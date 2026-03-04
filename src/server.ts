a39eae9882f93b7239d65de82cd787287f48f58b
Sayak-Mallick <sayakmallickkv@gmail.com>
2026-03-05 00:18:40 +0530

    [FIX] :  code cleaned

--- src/server.ts
+++ src/server.ts
@@ -2,7 +2,6 @@ import express, { Application, NextFunction, Request, Response } from 'express';
 
 const app: Application = express();
 app.use(express.json());
-const MONGO_URI = process.env.MONGO_URI || 'mongodb://localhost:27017/mydatabase';
 
 app.get('/', (req: Request, res: Response, next: NextFunction) => {
   res.json({
